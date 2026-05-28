aafe5faf36d8ce2f09bcabc256d0d7cc9896d0ed
Jihwan <gycha0109@gmail.com>
2026-05-28 12:06:04 +0900

    add pagination to question board

--- PROJECT1/questions/views.py
+++ PROJECT1/questions/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 from .forms import QuestionForm, SignupForm
 from .models import Question, Tag
 
@@ -9,14 +9,14 @@ def home(request):              # 홈 페이지
 
 
 def board(request):             # 질문 목록 페이지
-    sort = request.GET.get("sort", "latest")
+    sort = request.GET.get("sort", "latest")        
     tag_id = request.GET.get("tag")
     query = request.GET.get("q", "")
 
-    questions = (
+    questions = (                   # 질문 목록을 가져올 때 태그 정보와 공감 수를 함께 가져오기 위해 prefetch_related와 annotate 사용
         Question.objects
-        .prefetch_related("tags")
-        .annotate(agree_count=Count("agrees"))
+        .prefetch_related("tags")       # 질문과 관련된 태그 정보를 미리 가져와서 DB 쿼리 수를 줄임
+        .annotate(agree_count=Count("agrees"))  # 각 질문에 대한 공감 수를 계산하여 agree_count 필드로 추가
     )
     
     if query:
@@ -29,14 +29,19 @@ def board(request):             # 질문 목록 페이지
         questions = questions.order_by("-agree_count", "-created_at")
     else:
         questions = questions.order_by("-created_at")
+
+    paginator = Paginator(questions, 10)  # 페이지당 10개의 질문을 보여줌
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     
     tags = Tag.objects.all()
             
     return render(request, "questions/board.html", {
-        "questions": questions,
+        "questions": page_obj,
+        "page_obj": page_obj,
         "tags": tags,
         "selected_tag_id": tag_id,
-        "sort": sort,
+        "current_sort": sort,
         "query": query,
     })
 
