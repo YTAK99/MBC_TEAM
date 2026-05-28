7fbb2cddadb55d94f4481e7ac3df40a057ea86d7
youngin <cmeos50901@naver.com>
2026-05-28 12:56:48 +0900

    회원가입 수정

--- PROJECT1/classq/settings.py
+++ PROJECT1/classq/settings.py
@@ -72,19 +72,12 @@ DATABASES = {
 # 비밀번호 검증
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        }
     },
 ]
-
 # 언어/시간
 LANGUAGE_CODE = "ko-kr"
 
