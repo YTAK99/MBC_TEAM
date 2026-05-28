e00c62f6255c2478dead7b9cc902fd6101b82b17
Jihwan <gycha0109@gmail.com>
2026-05-28 12:43:49 +0900

    board make complete.. maybe

--- PROJECT1/questions/views.py
+++ PROJECT1/questions/views.py
@@ -1,8 +1,9 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .forms import QuestionForm, SignupForm
 from .models import Question, Tag
+from urllib.parse import urlencode
 
 def home(request):              # 홈 페이지
     return render(request, "questions/home.html")
@@ -10,7 +11,7 @@ def home(request):              # 홈 페이지
 
 def board(request):             # 질문 목록 페이지
     sort = request.GET.get("sort", "latest")        
-    tag_id = request.GET.get("tag")
+    selected_tag_ids = request.GET.getlist("tag")
     query = request.GET.get("q", "")
 
     questions = (                   # 질문 목록을 가져올 때 태그 정보와 공감 수를 함께 가져오기 위해 prefetch_related와 annotate 사용
@@ -20,27 +21,59 @@ def board(request):             # 질문 목록 페이지
     )
     
     if query:
-        questions = questions.filter(title__icontains=query)
-
-    if tag_id:
-        questions = questions.filter(tags__id=tag_id)
+        questions = questions.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query)
+    )
+    if selected_tag_ids:
+        for tag_id in selected_tag_ids:
+            questions = questions.filter(tags__id=tag_id)
 
     if sort == "popular":
         questions = questions.order_by("-agree_count", "-created_at")
     else:
         questions = questions.order_by("-created_at")
 
-    paginator = Paginator(questions, 10)  # 페이지당 10개의 질문을 보여줌
+    tags = Tag.objects.all()
+
+    tag_filters = []
+
+    for tag in tags:
+        tag_id = str(tag.id)
+        next_tag_ids = selected_tag_ids.copy()
+
+        if tag_id in next_tag_ids:
+            next_tag_ids.remove(tag_id)
+        else:
+            next_tag_ids.append(tag_id)
+
+        params = []
+
+        if sort:
+            params.append(("sort", sort))
+
+        if query:
+            params.append(("q", query))
+
+        for selected_id in next_tag_ids:
+            params.append(("tag", selected_id))
+
+        tag_filters.append({
+            "tag": tag,
+            "is_selected": tag_id in selected_tag_ids,
+            "url": "?" + urlencode(params),
+        })
+
+    paginator = Paginator(questions, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    
-    tags = Tag.objects.all()
-            
+
     return render(request, "questions/board.html", {
         "questions": page_obj,
         "page_obj": page_obj,
         "tags": tags,
-        "selected_tag_id": tag_id,
+        "tag_filters": tag_filters,
+        "selected_tag_ids": selected_tag_ids,
         "current_sort": sort,
         "query": query,
     })
